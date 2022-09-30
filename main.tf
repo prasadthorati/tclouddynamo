40f88a2e83b101a1686352c4ab7cf04c7a42ba72
prasadthorati <91524881+prasadthorati@users.noreply.github.com>
2022-09-30 15:58:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,4 @@ provider "aws" {
 module "dyna" {
   source  = "app.terraform.io/Prasad_org/dyna/aws"
   version = "1.0.0"
-name           = "DynamoDB-Terraform"
-billing_mode   = "PROVISIONED"
-read_capacity  = 20
-write_capacity = 20
-hash_key       = "UserId"
-range_key      = "Name"
 }
